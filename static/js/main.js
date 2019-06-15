a5f03f2982c3eb5c60fd1f59d4dc69c223631ef2
tbuck09 <tylerbuck09@gmail.com>
2019-06-14 21:04:49 -0600

    first heroku commit

--- static/js/main.js
+++ static/js/main.js
@@ -28,50 +28,7 @@ function loadMetadataTable(sample) {
 };
 
 
-// Define function for creating a Pie Chart for  the requested sample
-function createPieChart(sample) {
-    var url= `/samples/${sample}`;
-    d3.json(url).then(function(sampleDataEnter) {
-        var sampleData= {};
-        Object.entries(sampleDataEnter).forEach(([key,value]) => {
-            var topTen= value.slice(0,10);
-            sampleData[key]= topTen;
-        });
-        var trace= {
-            labels: sampleData.otu_ids,
-            values: sampleData.sample_values,
-            type: "pie",
-            hoverinfo: "text",
-            hovertext: sampleData.otu_labels
-        };
-        var sampleTrace= [trace];
-        Plotly.newPlot("pie", sampleTrace);
-    });
-};
-
-
-// Define function for creating Bubble Chart for the requested sample
-function createBubbleChart(sample) {
-    var url= `/samples/${sample}`;
-    d3.json(url).then(function(sampleDataEnter) {
-        var sampleData= sampleDataEnter;
-        var adjustedSizes= sampleData.otu_ids.map(n => n/100);
-        var trace= {
-            x: sampleData.otu_ids,
-            y: sampleData.sample_values,
-            mode: "markers",
-            marker: {
-                size: adjustedSizes,
-                color: sampleData.otu_ids
-            },
-            hovertext: sampleData.otu_labels 
-        };
-        var sampleTrace= [trace];
-        Plotly.newPlot("bubble", sampleTrace);
-    });
-};
-
-
+// Create charts in a single function
 function createCharts(sample) {
     var url= `/samples/${sample}`;
     d3.json(url).then(function(sampleData) {
@@ -99,10 +56,10 @@ function createCharts(sample) {
             x: sampleData.otu_ids,
             y: sampleData.sample_values,
             mode: "markers",
-            colorscale: "Jet",
             marker: {
                 size: adjustedSizes,
-                color: sampleData.otu_ids
+                color: sampleData.otu_ids,
+                colorscale: "Earth"
             }
             // text: [sampleData.otu_ids,sampleData.sample_values,sampleData.otu_labels],
         };
@@ -114,7 +71,7 @@ function createCharts(sample) {
 
 // Set variables for use in below functions
 var sampleSelection= d3.select("#sample-select-box");
-var sampleValue= ""
+var sampleValue= "940"
 
 // Create event-listener for changing the page based on 
 sampleSelection.on("change", function() {
@@ -131,25 +88,6 @@ sampleSelection.on("change", function() {
 // Load the select element
 loadMetadataSelect();
 
-
-
-
-var strArr= [];
-
-d3.json(url).then(function(sampleData) {
-    var sampleData= sampleData;
-    for (i=0; i < sampleData['otu_ids'].length; i++) {
-        var strEntry= ""
-        Object.keys(sampleData).forEach(key => {
-			var j = 0;
-			if (j < 2) {
-                strEntry.concat(String(data[key][i])).concat("<br>");
-                j++
-            } else if (j == 2) {
-                strEntry.concat(String(data[key][i]));
-                j++
-            };
-        strArr.push(strEntry);
-        });
-    };
-});
+// Create charts upon Load for default sample (940)
+loadMetadataTable(sampleValue);
+createCharts(sampleValue);
